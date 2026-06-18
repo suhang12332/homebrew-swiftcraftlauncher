769416f1dc98f0a33800abdfbd0579965f90980e
suhang12332 <48873455+suhang12332@users.noreply.github.com>
2026-06-18 19:02:43 +0800

    Update swift-craft-launcher.rb

--- Casks/swift-craft-launcher.rb
+++ Casks/swift-craft-launcher.rb
@@ -1,15 +1,17 @@
 cask "swift-craft-launcher" do
   version "1.2.3"
 
+  arch arm: "arm64", intel: "x86-64"
+
   on_arm do
-    sha256 :no_check
-    url "https://github.com/suhang12332/Swift-Craft-Launcher/releases/download/#{version}/Swift-Craft-Launcher-arm64-#{version}.dmg"
+    sha256 "b0a399b9697cdfa42e3662285e78d15f9d171524a99eef78b5eaae74225cce57"
   end
 
   on_intel do
-    sha256 :no_check
-    url "https://github.com/suhang12332/Swift-Craft-Launcher/releases/download/#{version}/Swift-Craft-Launcher-x86-64-#{version}.dmg"
+    sha256 "2dab12f0ded4a729ca39478f847a208cc747fb6f15bb283947e002a916ccaaae"
   end
+
+  url "https://github.com/suhang12332/Swift-Craft-Launcher/releases/download/#{version}/Swift-Craft-Launcher-#{arch}-#{version}.dmg"
 
   name "Swift Craft Launcher"
   desc "Modern Minecraft launcher for macOS built with SwiftUI"
@@ -19,6 +21,8 @@ cask "swift-craft-launcher" do
     url :url
     strategy :github_latest
   end
+
+  auto_updates true
 
   depends_on macos: :sonoma
 
